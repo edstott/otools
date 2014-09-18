cfbd04ad8164fa5fddb38647e5cbb2873c726ee7
Ed Stott <estott@imperial.ac.uk>
2014-09-18 15:27:09 +0100

    Creates osm multipolygons
    Startd contour writer

--- osgml2osm/osgml2osm.py
+++ osgml2osm/osgml2osm.py
@@ -1,107 +1,190 @@
 
 import xml.etree.ElementTree as ElementTree
 import pyproj
+import os
+
+outdir = 'output'
+bng = pyproj.Proj(init='epsg:27700')
+wgs84 = pyproj.Proj(init='epsg:4326')
 
 class OSMLayer:
 	root = None
 	bounds = None
 	tree = None
 	wayroot = None
+	relroot = None
 	name = ''
 	code = '0'
 	featurecount = 0
 	ftype = ''
+	minlat = 180.0
+	maxlat = -180.0
+	minlon = 90.0
+	maxlon = -90.0
 
 	def __init__(self,code,name):
 		self.root = ElementTree.Element('osm',{'version':'0.6','generator':'osgml2osm'})
 		self.bounds = ElementTree.Element('bounds',{'minlat':'0','minlon':'0','maxlat':'0','maxlon':'0'})
 		self.tree = ElementTree.ElementTree()
 		self.wayroot = ElementTree.Element('osm')
+		self.relroot = ElementTree.Element('osm')
 		self.tree._setroot(self.root)
 		self.root.append(self.bounds)
 		self.code = code
 		self.name = name
 
-	def setbounds(self,maxlat,maxlon,minlat,minlon):
-		self.bounds.set('maxlat',str(maxlat))
-		self.bounds.set('minlat',str(minlat))
-		self.bounds.set('maxlon',str(maxlon))
-		self.bounds.set('minlon',str(minlon))
+	def setbounds(self):
+		self.bounds.set('maxlat',str(self.maxlat))
+		self.bounds.set('minlat',str(self.minlat))
+		self.bounds.set('maxlon',str(self.maxlon))
+		self.bounds.set('minlon',str(self.minlon))
 
 	def write(self):
+		self.setbounds()
 		self.root.extend(self.wayroot)
-		self.tree.write(self.code+'-'+self.ftype+'-'+self.name.replace(' ','_')+'.osm','UTF-8')
+		self.root.extend(self.relroot)
+		self.tree.write(outdir+'/'+self.code+'-'+self.ftype+'-'+self.name.replace(' ','_')+'.osm','UTF-8')
+		
+	def addway(self,polyelem):
+		global idcount
+		
+		#Get coordinates
+		nodelist = polyelem.text.split()
+		east = [float(i.split(',')[0]) for i in nodelist]
+		north = [float(i.split(',')[1]) for i in nodelist]
+		(lon,lat) = pyproj.transform(bng,wgs84,east,north)
+		if max(lat)>self.maxlat:
+			self.maxlat = max(lat)
+		if min(lat)<self.minlat:
+			self.minlat = min(lat)
+		if max(lon)>self.maxlon:
+			self.maxlon = max(lon)
+		if min(lon)<self.minlon:
+			self.minlon = min(lon)
+		
+		#Create a new way element
+		way = ElementTree.Element('way',{'id': str(idcount)})
+		wayid = idcount
+		idcount += 1
+	
+		for (x,y) in zip(lat,lon):
+			self.root.append(ElementTree.Element('node',{'id':str(idcount), 'lat':str(x), 'lon':str(y)}))
+			way.append(ElementTree.Element('nd',{'ref':str(idcount)}))
+			idcount += 1
+	
+		self.wayroot.append(way)
+		self.featurecount += 1
+		return wayid
+			
+	def addrel(self,rel):
+		self.relroot.append(rel)
 		
 		
-buildings = ('10021')
-others = ('10185')
 
-bng = pyproj.Proj(init='epsg:27700')
-wgs84 = pyproj.Proj(init='epsg:4326')
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
 
 srctree = ElementTree.parse('test.gml')
 srcroot = srctree.getroot()
 print srcroot.tag
 
 idcount = 0
-featurecount = 0
-minlat = 180.0
-maxlat = -180.0
-minlon = 90.0
-maxlon = -90.0
 typedict = {}
 
-for feature in srcroot.iter('{http://www.ordnancesurvey.co.uk/xml/namespaces/osgb}topographicMember'):
-	featuretype = feature.find('{http://www.ordnancesurvey.co.uk/xml/namespaces/osgb}TopographicLine')
-	ftype = 'line'
-	if featuretype == None:
-		featuretype = feature.find('{http://www.ordnancesurvey.co.uk/xml/namespaces/osgb}TopographicArea')
-		ftype = 'area'
-	if featuretype == None:
-		featuretype = feature.find('{http://www.ordnancesurvey.co.uk/xml/namespaces/osgb}TopographicPoint')
-		ftype = 'point'
-	if featuretype == None:
-		break
+#Look for line features
+for linefeature in srcroot.iter('{http://www.ordnancesurvey.co.uk/xml/namespaces/osgb}TopographicLine'):
 
-
-	for desccode in featuretype.iter('{http://www.ordnancesurvey.co.uk/xml/namespaces/osgb}featureCode'):
-		code = desccode.text
+	#Get feature code
+	codeelem = linefeature.find('{http://www.ordnancesurvey.co.uk/xml/namespaces/osgb}featureCode')
+	if not codeelem == None:
+		code = codeelem.text
+		
+		#Check if feature code has been seen before, create new layer tree if not
 		if not code in typedict:
-			desctype = featuretype.find('{http://www.ordnancesurvey.co.uk/xml/namespaces/osgb}descriptiveGroup')
+			desctype = linefeature.find('{http://www.ordnancesurvey.co.uk/xml/namespaces/osgb}descriptiveGroup')
 			if not desctype == None:
 				desctext = desctype.text
 			else:
 				desctext = 'none'
 			typedict[code] = OSMLayer(code,desctext)
-			typedict[code].ftype = ftype
+			typedict[code].ftype = 'line'
 
 
-		for polygon in featuretype.iter('{http://www.opengis.net/gml}coordinates'):
-			nodelist = polygon.text.split()
-			way = ElementTree.Element('way',{'id': str(idcount)})
-			idcount += 1
-			east = [float(i.split(',')[0]) for i in nodelist]
-			north = [float(i.split(',')[1]) for i in nodelist]
-			(lon,lat) = pyproj.transform(bng,wgs84,east,north)
-			if max(lat)>maxlat:
-				maxlat = max(lat)
-			if min(lat)<minlat:
-				minlat = min(lat)
-			if max(lon)>maxlon:
-				maxlon = max(lon)
-			if min(lon)<minlon:
-				minlon = min(lon)				
+		for polygon in linefeature.iter('{http://www.opengis.net/gml}coordinates'):
+			typedict[code].addway(polygon)
 
-			for (x,y) in zip(lat,lon):
-				typedict[code].root.append(ElementTree.Element('node',{'id':str(idcount), 'lat':str(x), 'lon':str(y)}))
-				way.append(ElementTree.Element('nd',{'ref':str(idcount)}))
-				idcount += 1
+#Look for point features
+for pointfeature in srcroot.iter('{http://www.ordnancesurvey.co.uk/xml/namespaces/osgb}TopographicPoint'):
+
+	#Get feature code
+	codeelem = pointfeature.find('{http://www.ordnancesurvey.co.uk/xml/namespaces/osgb}featureCode')
+	if not codeelem == None:
+		code = codeelem.text
+		
+		#Check if feature code has been seen before, create new layer tree if not
+		if not code in typedict:
+			desctype = pointfeature.find('{http://www.ordnancesurvey.co.uk/xml/namespaces/osgb}descriptiveGroup')
+			if not desctype == None:
+				desctext = desctype.text
+			else:
+				desctext = 'none'
+			typedict[code] = OSMLayer(code,desctext)
+			typedict[code].ftype = 'point'
+
+
+		for polygon in pointfeature.iter('{http://www.opengis.net/gml}coordinates'):
+			typedict[code].addway(polygon)
+
+#Look for area features
+for areafeature in srcroot.iter('{http://www.ordnancesurvey.co.uk/xml/namespaces/osgb}TopographicArea'):
+	
+	#Get feature code
+	codeelem = areafeature.find('{http://www.ordnancesurvey.co.uk/xml/namespaces/osgb}featureCode')
+	if not codeelem == None:
+		code = codeelem.text
+		
+		#Check if feature code has been seen before, create new layer tree if not
+		if not code in typedict:
+			desctype = areafeature.find('{http://www.ordnancesurvey.co.uk/xml/namespaces/osgb}descriptiveGroup')
+			if not desctype == None:
+				desctext = desctype.text
+			else:
+				desctext = 'none'
+			typedict[code] = OSMLayer(code,desctext)
+			typedict[code].ftype = 'area'
 			
-			typedict[code].wayroot.append(way)
-			typedict[code].featurecount += 1
+		rel = ElementTree.Element('relation',{'id': str(idcount)})
+		rel.append(ElementTree.Element('tag',{'k':'type','v':'multipolygon'}))
+		idcount += 1
+		waycount = 0
+
+		#Get outer polygons
+		for outpoly in areafeature.iter('{http://www.opengis.net/gml}outerBoundaryIs'):
+			ringelem = outpoly.find('{http://www.opengis.net/gml}LinearRing')
+			if not ringelem == None:
+				polyelem = ringelem.find('{http://www.opengis.net/gml}coordinates')
+				if not polyelem == None:
+					wayid = typedict[code].addway(polyelem)
+					rel.append(ElementTree.Element('member',{'type':'way','ref':str(wayid),'role':'outer'}))
+					waycount += 1
+		
+		#Get inner polygons
+		for inpoly in areafeature.iter('{http://www.opengis.net/gml}innerBoundaryIs'):
+			ringelem = inpoly.find('{http://www.opengis.net/gml}LinearRing')
+			if not ringelem == None:
+				polyelem = ringelem.find('{http://www.opengis.net/gml}coordinates')
+				if not polyelem == None:
+					wayid = typedict[code].addway(polyelem)
+					rel.append(ElementTree.Element('member',{'type':'way','ref':str(wayid),'role':'inner'}))	
+					waycount += 1
+					
+		if waycount > 1:
+			typedict[code].addrel(rel)
+		
+		
+
 
 for layer in typedict:
 	print typedict[layer].name, typedict[layer].ftype, typedict[layer].featurecount, 'items'
-	typedict[layer].setbounds(str(maxlat),str(maxlon),str(minlat),str(minlon))
 	typedict[layer].write()
 	
